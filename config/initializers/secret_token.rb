17c97d2a53e1cdb94dfee6f9e2c00bc3b27a4f1d
func09 <mitsuru.haga@gmail.com>
2011-11-29 13:49:25 +0900

    empty rails project!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtndGrouping::Application.config.secret_token = '982a3492ef0624322b34170b3de660e1c9b25dcac52d20a1698fd8c99b8bf55d136e5885a212194d8646fdc64ed0bb898014058d3e16dd968ad6d87df513cff4'
