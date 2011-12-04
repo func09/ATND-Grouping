d058cc2510681278d7ca052bfca158eadcf539b9
func09 <mitsuru.haga@gmail.com>
2011-12-04 11:09:15 +0900

    Create groupings new show

---
+++ app/controllers/groupings_controller.rb
@@ -0,0 +1,8 @@
+class GroupingsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+end
---
+++ spec/views/groupings/new.html.haml_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "groupings/new.html.haml" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
